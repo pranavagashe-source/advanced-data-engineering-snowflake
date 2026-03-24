c27480a45066a80497f7d0d8a7175cff6d4a3f10
pranav.agashe.1984 <pranav.agashe@gmail.com>
2026-03-24 04:43:22 -0400

    CREATE OR ALTER

--- module-1/hamburg_weather/pipeline/data/load_tasty_bytes.sql
+++ module-1/hamburg_weather/pipeline/data/load_tasty_bytes.sql
@@ -57,7 +57,7 @@ raw zone table build
 -- country table build
 
 -- todo: complete table build
-CREATE TABLE {{env}}_tasty_bytes.raw_pos.country
+CREATE OR ALTER TABLE {{env}}_tasty_bytes.raw_pos.country
 (
    country_id NUMBER(18,0),
    country VARCHAR(16777216),
@@ -294,7 +294,7 @@ USE WAREHOUSE demo_build_wh;
 
 
 -- country table load
-COPY INTO {{env}}_tasty_bytes.raw_pos.country 
+COPY INTO {{env}}_tasty_bytes.raw_pos.country
 FROM @{{env}}_tasty_bytes.public.s3load/raw_pos/country/;
 
 
